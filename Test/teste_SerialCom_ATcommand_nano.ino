3ac3095449ce153c328cfea97c83a5fb23bda80a
Wararray <dias.unb@gmail.com>
2018-09-20 11:40:16 -0300

    Update test files

--- Test/teste_SerialCom_ATcommand_nano.ino
+++ Test/teste_SerialCom_ATcommand_nano.ino
@@ -1,60 +1,58 @@
-/* Comments and explanation done by Brandon Dias at First Integrated Command of Air Defense and Air Traffic - Defense Department
+/* Author: Brandon Dias 
+ * Developer at First Integrated Command of Air Defense and Air Traffic - Defense Department of Brazil
+ * Control and Automation Engineering Student
  * Contact: dias.unb@gmail.com
  */
 #include <SoftwareSerial.h>
 
-#define RX 4
-#define TX 5
+#define RX 4                            //After some trial and error     
+#define TX 5                            //Those are the two ports that worked
 
-int     countTrueCommand;
-int     countTimeCommand; 
-int     valSensor = 1;
-boolean found = false; 
+int     countValidResponse;             //Check How many commands returned valid with specific tag (usually "OK")
+int     countTimeResponse;              //Something to ensure not wait forever 
+boolean found = false;                  //A control flag
 
-SoftwareSerial esp8266(RX,TX); 
-   
+SoftwareSerial esp8266(RX,TX);          //Initialize the virtual port. Don't forget: Rx1 <---> Tx2
+                                        //                                           Tx1 <---> Rx2
 void setup() {
-    Serial.begin(9600);
-    esp8266.begin(115200);
-    sendCommand("AT",5,"OK");
+    Serial.begin(9600);                 //Monitoring activity from Serial Monitor at 9600 baud            
+    esp8266.begin(115200);              //Start comunication with module. Usually fabricant rate: 115200 baud
+    sendCommand("AT",5,"OK");           //First atempt to comunicate: Sending "AT" and waiting "OK" as response 
 }
 
 void loop() {
-    sendCommand("",1,"");
-    delay(1000);
-    sendCommand("AT",5,"OK");
-    delay(2000);                // Important to program don't get stuck 
+    sendCommand("",1,"");               //Ensure that this program is working
+    delay(1000);                        //Some erros of comunication can be avoided just giving more time to things happen
+    sendCommand("AT",5,"OK");           //Attempt to comunicate
+    delay(2000);                        //Important to program don't get stuck; Same reason as before
 }
 
+//This function was taken (part of it) from some random forum and, unfurtunelly, there is no author descrived there.
 void sendCommand(String command, int maxTime, char readReplay[]) {
   
-    Serial.print(countTrueCommand);
+    Serial.print(countValidResponse);
     Serial.print(". at command => ");
     Serial.print(command);
     Serial.print(" ");
     
-    while(countTimeCommand < (maxTime*1))  {
+    while(countTimeResponse < (maxTime*1))  {
         esp8266.println(command);                      //command = AT
-  //      Serial.print("Dentro do While");
-
         if(esp8266.find(readReplay)){                  //Searching for "OK"
-  //      Serial.print("Preso no Esp?");
             found = true;
-            break;
+            break;                                     //Remember to change this ugly thing
         }  
-        countTimeCommand++;
+        countTimeResponse++;
     }
     
     if(found == true){
         Serial.println("Done");
-        countTrueCommand++;
-        countTimeCommand = 0;
+        countValidResponse++;
+        countTimeResponse = 0;
     }    
     if(found == false){
         Serial.println("Fail");
-        countTrueCommand = 0;
-        countTimeCommand = 0;
+        countValidResponse = 0;
+        countTimeResponse = 0;
     }    
     found = false;
-//    Serial.print("Final da Funcao\n");
 }
